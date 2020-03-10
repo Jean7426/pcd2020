2549cb6b6f1d3a45ebd3cee926c0b5642e97ea74
Jean7426 <jean.jpls@hotmail.com>
2020-03-09 21:03:19 -0400

    Exemplo de Olá Mundo - 2020.03.09 - 22:02

---
+++ 000-ola_mundo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+##
+# Dependencias
+##
+# pip3 install ipadress
+
+##
+# Modulos
+##
+import ipaddress
+import socket
+import sys
+
+#print("Olá Mundo !!!")
+sys.stdout.write ("\nOlá Mundo \n\n")
+sys.stderr.write ("\n\t\tDeu ruim !!!\n")
